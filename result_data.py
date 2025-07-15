e8cf5ff0b764e728f64fee211b1866b9da7c9c7d
HelenTorchina <helentrufanov@gmail.com>
2025-07-15 11:42:49 +0400

    Add files via upload

---
+++ result_data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+
+# Параметры
+
+def create_result():
+    n_per_group = 87000  # размер группы
+    base_churn = 0.018  # базовый отток (1.8%)
+    expected_reduction = 0.0018  # минимальное снижение оттока (0.18 п.п.)
+
+    # Сгенерируем данные для группы A (контроль)
+    group_A_churn = np.random.binomial(1, base_churn, n_per_group)
+
+    # Сгенерируем данные для группы B (тест)
+    group_B_churn = np.random.binomial(1, base_churn - expected_reduction, n_per_group)
+
+    # Используем биномиальное распределение, т.к. у нас два исхода: игрок остался либо ушёл
+
+    #   Соберем в один DataFrame
+    df = pd.DataFrame({
+        'group': ['A'] * n_per_group + ['B'] * n_per_group,
+        'churned': np.concatenate([group_A_churn, group_B_churn])
+    })
+
+    # Посмотрим на сводку
+    summary = df.groupby('group')['churned'].mean()
+    print(summary)
+
+    # Сохраним файл
+    output_path = "D:/AB test/ab_test_churn_data.csv"
+    df.to_csv(output_path, index=False)
