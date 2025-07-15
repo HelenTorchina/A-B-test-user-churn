67d10148cefc3626559c753e4dea1375a642f7e5
HelenTorchina <helentrufanov@gmail.com>
2025-07-15 15:18:16 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,59 +2,55 @@ import pandas as pd
 from IPython.display import display
 import seaborn as sns
 from matplotlib import pyplot as plt
-from statsmodels.stats.proportion import proportions_ztest, proportion_confint
+import result_data
+import AB_test
 
-
-def main():
-    df = pd.read_csv('input_data.csv', sep=';')
-    # df = df['SawStep;cry_per_user;LRC;churn'].str.split(';', expand=True)
-    # df.rename(columns={0: "SawStep", 1: "cry_per_user", 2: "LRC", 3: "churn"}, inplace=True)
+def analysis():
     df.info()
     display(df)
+    
+    # Приведение типов данных
     df['SawStep'] = df['SawStep'].astype('int64')
     df['cry_per_user'] = df['cry_per_user'].astype('float')
     df['LRC'] = df['LRC'].astype('float')
     df['churn'] = df['churn'].astype('float')
     df.info()
     print(df.describe())
-    # print(df.duplicated().sum())
-    # sns.boxplot(x=df['SawStep'])
-    # plt.show()
-    # sns.boxplot(x=df['cry_per_user'])
-    # plt.show()
-    # sns.boxplot(x=df['LRC'])
-    # plt.show()
-    # Q1 = df['LRC'].quantile(0.25)
-    # Q3 = df['LRC'].quantile(0.75)
-    # IQR = Q3 - Q1
-    # lower_bound = Q1 - 1.5 * IQR
-    # outliers = df[(df['LRC'] < lower_bound)]
-    # display(outliers)
-    # #низкий уровень сложности, небольшое количество полученной игровой валюты и небольшой отток - всё логично
-    # sns.boxplot(x=df['churn'])
-    # plt.show()
+    
+    # Проверка на дубликаты
+    print(df.duplicated().sum())
+    #Дубликатов не обнаружено
+
+    # Ищем выбросы - они подскажут, есть ли проблемы в балансе твёрдой валюты и оттока
+    sns.boxplot(x=df['SawStep'])
+    plt.show()
+    # Выбросов нет
+    sns.boxplot(x=df['cry_per_user'])
+    plt.show()
+    # Выбросов нет
+    sns.boxplot(x=df['LRC'])
+    plt.show()
+    # Выбросы по уровню сложности - очень маленький уровень сложности
+    Q1 = df['LRC'].quantile(0.25)
+    Q3 = df['LRC'].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    outliers = df[(df['LRC'] < lower_bound)]
+    display(outliers)
+    # Низкий уровень сложности, небольшое количество полученной игровой валюты и небольшой отток - всё логично и не вредит системе
+    sns.boxplot(x=df['churn'])
+    plt.show()
+    # Выбросы в большую сторону наблюдаются, на этих шагах высокий отток, выведем информацию о них (в том числе в отдельный файл) и подумаем, как можно сократить отток на этих шагах
     Q1 = df['churn'].quantile(0.25)
     Q3 = df['churn'].quantile(0.75)
     IQR = Q3 - Q1
     upper_bound = Q3 + 1.5 * IQR
     outliers_churn = df[(df['churn'] > upper_bound)]
     display(outliers_churn)
+    # Посмотрим средний отток и уровень сложности на этих шагах, затем сравним со средним оттоком и уровнем сложности по выборке (0,006 и 0,6162)
     print(outliers_churn.describe())
-    # подозрительными выглядят пара выбросов: 1209 и 1071, думаю, можно забить
-
-
-    # Посмотрим графики, как изменяется каждый показатель в зависимости от шага
-    plt.figure(figsize=(12, 6))
-    sns.lineplot(x='SawStep', y='cry_per_user', data=df, label='Cry per user')
-    sns.lineplot(x='SawStep', y='LRC', data=df, label='LRC')
-    sns.lineplot(x='SawStep', y='churn', data=df, label='Churn')
-    plt.title('Динамика валюты, поражений и оттока по шагам')
-    plt.show()
-
-    # 1. Где резко растёт отток? Где выбросы в churn, т.е. outliers - вопрос, почему? Строки 1071 (шаг 2324) и 1209 (шаг 2604) вообще интересные, можно сделать А/В тест
-
-
-
+    # На данных шагах отток 0,018 при уровне сложности 0,7529
+    # 1. Здесь могу предложить проверку с помощью A/B теста, уменьшится ли на этих шагах отток при снижении уровня сложности, например, на 0,03 единицы
 
     # 2. Где большие траты, но маленькая сложность? Может быть, стоит поднять сложность
     df['cry_per_user_LRC_relation'] = df['cry_per_user']/df['LRC']
@@ -66,8 +62,7 @@ def main():
     upper_bound = Q3 + 1.5 * IQR
     outliers_cry_per_user_LRC_relation = df[(df['cry_per_user_LRC_relation'] > upper_bound)]
     display(outliers_cry_per_user_LRC_relation)
-    # Есть выбросы сверху, но отток здесь в пределах разумного, т.е. высокая стоимость соответствует высокой сложности и это устраивает пользователей
-
+    # Есть выбросы сверху, но отток здесь в пределах разумного, т.е. высокая стоимость соответствует высокой сложности и это устраивает пользователей, нет необходимости проводить дополнительный анализ
 
     # 3. Есть ли шаги, где игроки уходят, но не платят? Возможно, слишком сложно — можно снизить сложность или снизить стоимость попытки.
     df['churn_cry_per_user_relation'] = df['churn'] / df['cry_per_user']
@@ -79,21 +74,20 @@ def main():
     upper_bound = Q3 + 1.5 * IQR
     outliers_churn_cry_per_user_relation = df[(df['churn_cry_per_user_relation'] > upper_bound)]
     display(outliers_churn_cry_per_user_relation.sort_values(by='LRC', ascending=False))
-    # Получили 76 шагов, уберём те, где отток 0,015 и более, т.к. мы уже взяли эту группу в работу, и проанализируем оставшиеся
-    # Также нас не интересуют шаги с небольшим оттоком - 0,006 (это среднее значение по колонке) и менее
+    # Получили 76 шагов, уберём те, где отток 0,015 и более, т.к. мы уже взяли эту группу в работу на первом шаге, и проанализируем оставшиеся
+    # Также нас не интересуют шаги с небольшим оттоком - 0,006 (это среднее значение оттока по всей выборке) и менее
     outliers_churn_cry_per_user_relation_filtered = outliers_churn_cry_per_user_relation[
         (outliers_churn_cry_per_user_relation['churn'] < 0.015) & (outliers_churn_cry_per_user_relation['churn'] >= 0.006)]
     display(outliers_churn_cry_per_user_relation_filtered.sort_values(by='LRC', ascending=False))
     outliers_churn_cry_per_user_relation_filtered.sort_values(by='LRC', ascending=False).to_csv('output.csv', index=False)
-    # Нашли 55 таких шагов и можем предложить повысить цену либо понизить уровень
+    # Нашли 55 таких шагов и можем предложить повысить цену либо понизить уровень - можем провести ещё один А/В тест
 
 
-    # 4. Отношение стоимости к количеству ушедших - что оно показывает? Есть ли выбросы и хороши ли они (высокий показатель - отлично, много платят, мало уходят
-    # низкий показатель - плохо, ведь мало платят и много уходят
+    # 4. Отношение стоимости к количеству ушедших - что оно показывает? Есть ли выбросы и хороши ли они (высокий показатель - отлично, много платят, мало уходят, низкий показатель - плохо, ведь мало платят и много уходят
     df['cry_per_churn'] = df['cry_per_user'] / df['churn']
     sns.boxplot(x=df['cry_per_churn'])
     plt.show()
-    # Уже видим, что выбросы только в большую сторону, значит, здесь нет проблемы, гипотезы строить не будем
+    # Уже видим, что выбросы только в большую сторону, значит, здесь нет проблемы и нет смысла в дальнейшем анализе
     Q1 = df['cry_per_churn'].quantile(0.25)
     Q3 = df['cry_per_churn'].quantile(0.75)
     IQR = Q3 - Q1
@@ -102,55 +96,21 @@ def main():
     outliers_cry_per_churn.sort_values('churn', ascending=False).to_csv('output.csv', index=False)
     # Проверили размер оттоков здесь и убедились, что эти шаги нам не интересны (отток меньше 0,006)
 
-    #ab_test.create_result()
 
-    #Загружаем результат теста для дальнейшего анализа и интерпретации результата
+def main():
+    df = pd.read_csv('input_data.csv', sep=';')
+
+    # Запускаем анализ для определения возможных A/B тестов
+    processing(df)
+
+    # Генерируем результаты первого теста
+    result_data.create_result()
+
+    # Загружаем результат теста для дальнейшего анализа и интерпретации результата
     df_result = pd.read_csv('ab_test_churn_data.csv')
-    # Смотрим первые строки и статистику
-    print(df_result.head())
-    print(df_result['group'].value_counts())
-    print(df_result['churned'].value_counts())
-
-    # Таблица сопряжённости: сколько ушло / осталось в каждой группе
-    contingency_table = pd.crosstab(df_result['group'], df_result['churned'])
-    print("\nТаблица сопряжённости:")
-    print(contingency_table)
-
-    # Количество ушедших по группам
-    churn_counts = contingency_table[1].values
-    # Всего пользователей по группам
-    nobs = contingency_table.sum(axis=1).values
-
-    print(f"Ушло: {churn_counts}")
-    print(f"Всего: {nobs}")
-
-    # Z-тест
-    z_stat, p_val = proportions_ztest(count=churn_counts, nobs=nobs, alternative='larger')
-    # 'larger' — потому что мы проверяем, что конверсия в control больше, чем в test (ожидаем уменьшение)
-
-    print(f"\nZ-статистика: {z_stat:.3f}")
-    print(f"P-value: {p_val:.5f}")
-
-    # Доверительные интервалы для долей
-    confint_control = proportion_confint(count=churn_counts[0], nobs=nobs[0], alpha=0.05)
-    confint_test = proportion_confint(count=churn_counts[1], nobs=nobs[1], alpha=0.05)
-
-    rate_control = churn_counts[0] / nobs[0]
-    rate_test = churn_counts[1] / nobs[1]
-
-    print(f'\n Отток (control): {rate_control:.4%}, 95% CI: [{confint_control[0]:.4%}, {confint_control[1]:.4%}]')
-    print(f' Отток (test):    {rate_test:.4%}, 95% CI: [{confint_test[0]:.4%}, {confint_test[1]:.4%}]')
-
-    # Абсолютный эффект (разница)
-    diff = rate_control - rate_test
-    print(f'\n Абсолютное снижение оттока: {diff:.4%}')
-
-    # Финальный краткий вывод
-    alpha = 0.05
-    if p_val < alpha:
-        print('\n РЕЗУЛЬТАТ: есть статистически значимое снижение оттока! Нулевая гипотеза отклоняется.')
-    else:
-        print('\n РЕЗУЛЬТАТ: статистически значимого снижения оттока не найдено. Нулевая гипотеза не отклоняется.')
+    
+    # Проводим тест и делаем выводы
+    AB_test.AB_test(df_result)
 
 
 if __name__ == '__main__':
